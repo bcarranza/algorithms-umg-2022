46a8defb802ddd961dc022a5e7e762be47401d4f
bcarranza <bayron.carranza@gmail.com>
2022-06-18 11:53:03 -0600

    circle pseint + csharp

---
+++ semana2/circle_csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double radio = 0;
+        double superficie = 0;
+        double perimetro = 0;
+        const double pi = 3.1416;
+
+        Console.WriteLine("Introduce el radio de la circunferencia:");
+        radio = Convert.ToDouble(Console.ReadLine());
+        superficie =  Math.Pow(pi * radio,2);
+        perimetro = 2 * pi * radio;
+        Console.WriteLine("La superficie es: " + superficie);
+        Console.WriteLine("El perímetro es " + perimetro);
+    }
+}
+
+
+
---
+++ semana2/circle_pseint/circle.psc
@@ -0,0 +1,9 @@
+Algoritmo Circulo
+	Definir radio,superficie,perimetro como Real;
+    Escribir "Introduce el radio de la circunferencia:";
+    Leer radio;
+    superficie <- PI * radio ^ 2;
+    perimetro <- 2 * PI * radio;
+    Escribir "La superficie es ",superficie;
+    Escribir "El perímetro es ",perimetro;	
+FinAlgoritmo

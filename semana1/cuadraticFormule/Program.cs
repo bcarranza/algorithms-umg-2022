b86bb7eff492e89923927b9b7f760cfac92d568e
bcarranza <bayron.carranza@gmail.com>
2022-07-09 14:24:25 -0600

    add radio validations

--- semana1/cuadraticFormule/Program.cs
+++ semana1/cuadraticFormule/Program.cs
@@ -27,12 +27,17 @@ internal class Program
         
         //proceso
 
-        x_positivo = ((b*-1) + Math.Sqrt(Math.Pow(b,2) - 4*a*c))/2*a;
-        x_negativo = ((b*-1) - Math.Sqrt(Math.Pow(b,2) - 4*a*c))/2*a;
+        x_positivo = ((b*-1) + Math.Sqrt(Math.Pow(b,2) - (4*a*c)))/(2*a);
+        x_negativo = ((b*-1) - Math.Sqrt(Math.Pow(b,2) - (4*a*c)))/(2*a);
         
         Console.WriteLine(FiggleFonts.Ogre.Render("RESULTADOS"));
-        Console.WriteLine("x + = " + x_positivo.ToString());
-        Console.WriteLine("x - = " +x_negativo.ToString());
+        
+        Console.WriteLine("a + = " + a);
+        Console.WriteLine("b - = " + b);
+        Console.WriteLine("c - = " + c);
+
+        Console.WriteLine("x + = " + x_positivo);
+        Console.WriteLine("x - = " + x_negativo);
 
     }
 }
--- semana2/circle_csharp/Program.cs
+++ semana2/circle_csharp/Program.cs
@@ -10,6 +10,12 @@ internal class Program
 
         Console.WriteLine("Introduce el radio de la circunferencia:");
         radio = Convert.ToDouble(Console.ReadLine());
+        if(radio<0){
+            Console.WriteLine("No es posible calcular el permitro y superficie ya que el circulo tiene un radio imaginario");    
+        }
+        if(radio==0){
+            Console.WriteLine("No es posible calcular el permitro y superficie ya que parece ser que esto no correspondo a un circulo si no que a un solo punto");    
+        }
         superficie =  Math.Pow(pi * radio,2);
         perimetro = 2 * pi * radio;
         Console.WriteLine("La superficie es: " + superficie);

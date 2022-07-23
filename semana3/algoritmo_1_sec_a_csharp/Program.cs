471ca07046c6e39e267e8b6d69cff546b4a83a2e
bcarranza <bayron.carranza@gmail.com>
2022-07-23 10:54:36 -0600

    algoritmo 1 seccion a

---
+++ semana3/algoritmo_1_sec_a_csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿//Esto resuelve el algoritmo 1 de la hoja de trabajo
+// Seccion A
+using System.Globalization;
+using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //code here..
+        int a,b;
+        float r; 
+        const exponent = 2
+
+        C
+    }
+}

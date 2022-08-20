f59084f34229e740e6e0390275e72ce337518a77
bcarranza <bayron.carranza@gmail.com>
2022-08-20 10:10:46 -0600

    par o impar en csharp

---
+++ semana7/numeroparimpar_csharp_secA/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Autor: Bayron Carranza
+Github: bayron.carranza
+Descripción: Determina si un numero es par o impar usando MOD
+% = MOD
+*/
+
+using System;
+internal class Program
+{   
+    private static void Main(string[] args)
+    {
+      int num;
+      Console.WriteLine("Ingresar un numero par/impar: ");
+      num = Convert.ToInt32(Console.ReadLine());
+
+      if(num % 2 == 0){
+        Console.WriteLine("El numero " + num + " es par");
+      }
+      else{
+        Console.WriteLine("El numero " + num + " es impar");
+      }
+    }
+}

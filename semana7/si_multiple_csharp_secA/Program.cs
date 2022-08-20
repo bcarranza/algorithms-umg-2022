7c09121326f483a82dc06e2f3bb5bdf882a37b8a
bcarranza <bayron.carranza@gmail.com>
2022-08-20 11:24:42 -0600

    changes

---
+++ semana7/si_multiple_csharp_secA/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Autor: Bayron Carranza
+Github: bayron.carranza
+Descripción: Aplicacion de estructura selectiva multiple.
+*/
+
+using System;
+internal class Program
+{   
+    private static void Main(string[] args)
+    {
+        int num;
+        int valor;
+        int result;
+        Console.WriteLine("Escribe un numero");
+        num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Escribe un valor");
+        valor = Convert.ToInt32(Console.ReadLine());
+
+        switch(num) 
+        {
+            case 1:
+                result = 100*valor;
+                break;
+            case 2:
+                result = 100*valor*valor;
+                break;
+            case 3:
+                result = 100/valor;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+        Console.WriteLine("Resultado " + result);
+    }
+}

ec1e6bccf355d6573c3854ed978037be5259bbf8
Bayron Carranza <bayron.carranza@gmail.com>
2022-08-20 08:14:59 -0600

    Par o impar utilizando MOD

---
+++ semana7/numeroparimpar_csharp_secB/Program.cs
@@ -0,0 +1,26 @@
+﻿
+/*
+Autor: Bayron Carranza
+Github: bayron.carranza
+Descripción: Determina si un numero es par o impar utilizando el operador MOD
+*/
+
+using System;
+internal class Program
+{   
+    private static void Main(string[] args)
+    {
+        int numParImpar;
+        Console.WriteLine("Ingrese un numero");
+        numParImpar = Convert.ToInt32(Console.ReadLine());
+
+        if(numParImpar % 2 == 0)
+        {
+            Console.WriteLine("El numero " + numParImpar + " es par" );
+        }
+        else
+        {
+            Console.WriteLine("El numero " + numParImpar + " es impar" );
+        }
+    }
+}

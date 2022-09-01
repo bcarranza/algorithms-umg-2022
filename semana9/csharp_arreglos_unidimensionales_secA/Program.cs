1884b47cc4af751e5695aa020a5c6ae0165a3b12
bcarranza <bayron.carranza@gmail.com>
2022-08-31 23:10:17 -0600

    c# program

---
+++ semana9/csharp_arreglos_unidimensionales_secA/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+        int opc;
+        int buffer;
+        int[] list = new int[8];
+        opc=0;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("MENU DE ARREGLOS UNIDIMENSIONALES");
+            Console.WriteLine("1. Ingresar valores al arreglo");
+            Console.WriteLine("2. Imprimir valores del arreglo");
+            Console.WriteLine("3. Imprimir el valor mas alto del arreglo");
+            Console.WriteLine("4. Imprimir el valor mas bajo del arreglo");
+            Console.WriteLine("5. Imprimir la suma total del arreglo");
+            Console.WriteLine("6. Imprimir el arreglo al reves");
+            Console.WriteLine("7. Salir");
+
+            Console.WriteLine("Ingresar opcion del menu: ");
+            opc = Convert.ToInt32(Console.ReadLine());
+
+            switch (opc)
+            {
+                case 1:
+                    Console.WriteLine("Ingresando valores al arreglo");
+				    for (int i = 0; i < list.Length; i++)
+                    {
+                        Console.WriteLine("Ingrese un numero para la posición " + i);
+                        list[i]=Convert.ToInt32(Console.ReadLine());     
+                    }
+				    Console.WriteLine("Proceso finalizado...");
+                    break;
+                case 2:
+                    Console.WriteLine("Imprimiendo valores del arreglo");
+                    for (int i = 0; i < list.Length; i++)
+                    {
+                        Console.WriteLine("Posición [" + i + "]: " + list[i]);    
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Imprimiendo el valor mas alto del arreglo");
+				    buffer=0;
+                    for (int i = 0; i < list.Length; i++)
+                    {
+                        if (list[i]>buffer)
+                        {
+                            buffer=list[i];
+                        }
+                    }
+                    Console.WriteLine("El numero mayor es: " + buffer);
+                    break;
+                case 4:
+                    Console.WriteLine("Imprimiendo el valor mas bajo del arreglo");
+                    buffer=0;
+                    for (int i = 0; i < list.Length; i++)
+                    {   
+                        if (i==0)
+                        {
+                            buffer=list[i];
+                        }
+                        else
+                        {
+                            if (list[i]<buffer)
+                            {
+                                buffer=list[i];
+                            }    
+                        }
+                    }
+                    Console.WriteLine("El numero menor es: " + buffer);
+                    break;
+                case 5:
+                    Console.WriteLine("Imprimiendo la suma total del arreglo");
+                    buffer=0;
+                    for (int i = 0; i < list.Length; i++)
+                    {
+                        buffer=buffer+list[i];   
+                    }
+                    Console.WriteLine("La suma del arreglo es: " + buffer);
+                    break;
+                case 6:
+                    Console.WriteLine("Imprimiendo el arreglo al reves");
+                    for (int i = list.Length-1; i>=0; i--)
+                    {
+                        Console.WriteLine("Posicion [" + i +"]:" + list[i]);
+                    }
+                    break;
+                case 7:
+                    Console.WriteLine("Adios, gracias por usar el programa");
+                    break;
+                default:
+                    Console.WriteLine("Opps algo salio mal, ingrese una opcion de nuevo");
+                    break;
+            }
+            Console.ReadKey();
+        } while (opc!=7);
+    }
+}

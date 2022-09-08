441449ecb6ed3e59c1e72632a6eca24b0105b9d1
bcarranza <bayron.carranza@gmail.com>
2022-09-07 23:17:30 -0600

    tarea seccion A y seccion B

---
+++ extra/seccionB/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        int opc=0;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("MENU PRINCIPAL........");            
+            Console.WriteLine("----------------------");
+            Console.WriteLine("1. EJEMPLO DE WHILE...");            
+            Console.WriteLine("2. EJEMPLO DE DO-WHILE");
+            Console.WriteLine("3. EJEMPLO DE FOR-NEXT");
+            Console.WriteLine("4. SALIR..............");
+
+            Console.WriteLine("Ingrese su opción deseada: ");
+            opc = Convert.ToInt32(Console.ReadLine());
+
+            switch (opc) //selector
+            {
+                case 1:  //1. EJEMPLO DE WHILE... 
+                    Console.WriteLine("Ejectuando: Gastos de viaje");
+					Console.WriteLine("Supongamos que debemos obtener una suma de gastos");
+					Console.WriteLine("que hicimos en nuestro ultimo viaje, pero no sabemos");
+					Console.WriteLine("exactamente cuantos fueron. Los datos son expresados");
+					Console.WriteLine("Datos: Gasto1, Gasto2, Gasto3...GastoN, -1");
+					Console.ReadKey();
+
+                    double SumGas=0;
+                    double Gasto=0;
+
+                    while (Gasto!=-1)
+                    {
+                        Console.WriteLine("Ingresar nuevo gasto: ");
+                        Gasto= Convert.ToDouble(Console.ReadLine());
+                        SumGas = SumGas + Gasto;
+                        Console.WriteLine("Gasto acumulado: " + SumGas);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Mostrando: Ejemplo Do-While");
+					Console.WriteLine("Conteo de pares e impares");
+					Console.WriteLine("Ingresaremos un numero ");
+					Console.WriteLine("al final mostraremos cuantos numeros");
+					Console.WriteLine("pares y cuantos impares se ingresaron");
+					Console.ReadKey();
+
+                    int n=0,i=1,pares=0,impares=0;
+
+                    Console.WriteLine("Ingrese el tamano de la lista: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+
+                    do
+                    {
+                        if (i % 2 == 0 )
+                        {
+                            pares++;
+                        }
+                        else
+                        {
+                            impares++;
+                        }
+                        i++;
+                    } while (i<=n);
+
+                    Console.WriteLine("Resultados");
+					Console.WriteLine("Pares: " + pares);
+					Console.WriteLine("Impares: " + impares);
+                    break;
+                case 3:
+                    Console.WriteLine("Mostrando: Ejemplo For-Next");
+					Console.WriteLine("Dado como Datos N numeros enteros");
+					Console.WriteLine("Obtenga el numero de ceros que hay dentro de la lista");
+                    
+                    int m=0, x=0, y=0, CuentaCeros=0;
+					
+					Console.WriteLine("De que tamano deseas tu lista: ");
+					m = Convert.ToInt32(Console.ReadLine());
+					
+					for(x=1; x<=m; x++){
+						Console.WriteLine("Ingrese numero");
+						y = Convert.ToInt32(Console.ReadLine());
+						if(y==0){
+                           CuentaCeros++;
+                        }
+					}
+					Console.WriteLine("Usted ingreso cero " + CuentaCeros + " veces");
+                    break;
+                case 4:
+                    Console.WriteLine("Gracias por usar el sistema, vuelva pronto");
+                    break;
+                default:
+                    Console.WriteLine("Opcion incorrecta, ingrese una nueva");
+                    break;
+            }
+            Console.ReadKey();
+        } while (opc!=4);
+    }
+}
--- semana10/csharp_arreglos_unidimensionales_secB/Program.cs
+++ semana10/csharp_arreglos_unidimensionales_secB/Program.cs
@@ -5,26 +5,40 @@ public class Program
 	public static void Main()
 	{
         int opc;
-        int buffer;
-        int[] list = new int[8];
+        int buffer=0;
+        string bufferstr="";
+        int[] list=null;
+        int size=0;
+        int pares=0;
+        int impares=0;
         opc=0;
         do
         {
             Console.Clear();
             Console.WriteLine("MENU DE ARREGLOS UNIDIMENSIONALES");
+            Console.WriteLine("0. Definir tamaño del arreglo");
             Console.WriteLine("1. Ingresar valores al arreglo");
             Console.WriteLine("2. Imprimir valores del arreglo");
             Console.WriteLine("3. Imprimir el valor mas alto del arreglo");
             Console.WriteLine("4. Imprimir el valor mas bajo del arreglo");
             Console.WriteLine("5. Imprimir la suma total del arreglo");
             Console.WriteLine("6. Imprimir el arreglo al reves");
-            Console.WriteLine("7. Salir");
+            Console.WriteLine("7. Imprimir el resumen de números pares y números impares");
+            Console.WriteLine("8. Imprimir el arreglo elevado al cuadrado (cada elemento)");
+            Console.WriteLine("9. Imprimir los números primos");
+            Console.WriteLine("10. Salir");
 
             Console.WriteLine("Ingresar opcion del menu: ");
             opc = Convert.ToInt32(Console.ReadLine());
 
             switch (opc)
             {
+                
+                case 0:
+                    Console.WriteLine("Ingrese el tamaño del arreglo: ");
+                    size = Convert.ToInt32(Console.ReadLine());
+                    list = new int[size];
+                    break;
                 case 1:
                     Console.WriteLine("Ingresando valores al arreglo");
                     for (int i = 0; i < list.Length; i++) //0,1,2...7
@@ -89,6 +103,43 @@ public class Program
                     }
                     break;
                 case 7:
+                    Console.WriteLine("Resumen: Pares e impares");
+                    for (int i = 0; i < list.Length; i++)
+                    {
+                        if (list[i] % 2 == 0)
+                        {
+                          pares++;  
+                        }
+                        else
+                        {
+                           impares++; 
+                        }   
+                    }
+                    Console.WriteLine("Pares: " + pares);
+                    Console.WriteLine("Impares: " + impares);
+                    break;
+                case 8:
+                    bufferstr = "";
+                    Console.WriteLine("El arreglo al cuadrado");
+                    for (int i = 0; i < list.Length; i++)
+                    {
+                        bufferstr = ((i==list.Length-1) ? (bufferstr + Math.Pow(list[i],2).ToString()) : (bufferstr + Math.Pow(list[i],2).ToString() + ",")); 
+                    }
+                    Console.WriteLine("Resultado: " + bufferstr);
+                    break;
+                case 9:
+                    bufferstr=""; 
+                    Console.WriteLine("Resumen de los numeros primos");
+                    for (int i = 0; i < list.Length; i++)
+                    {
+                        if(EsPrimo(list[i]))
+                        {
+                            bufferstr = ((i==list.Length-1) ? (bufferstr + list[i].ToString()) : (bufferstr + list[i] + ",")); 
+                        }
+                    }
+                    Console.WriteLine("Resultado: " + bufferstr);
+                    break;
+                case 10:
                     Console.WriteLine("Adios, gracias por usar el programa");
                     break;
                 default:
@@ -96,6 +147,20 @@ public class Program
                     break;
             }
             Console.ReadKey();
-        } while (opc!=7);
+        } while (opc!=10);
     }
+    static bool EsPrimo(int numero)
+        {
+            for (int i = 2; i < numero; i++)
+            {
+                if((numero % i) == 0)
+                {
+                    // No es primo 🙁
+                    return false;
+                }
+            }
+
+            // Es primo 🙂
+            return true;
+        }
 }

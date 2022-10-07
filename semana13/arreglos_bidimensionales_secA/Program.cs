8385746474e3a1cd9b970583e22a68ba2b2112ac
bcarranza <bayron.carranza@gmail.com>
2022-10-06 18:52:07 -0600

    changes

---
+++ semana13/arreglos_bidimensionales_secA/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Selector de control de mi menu
+        int opc = 0;
+        
+        // Definir la dimensión de mis matrices
+        int filas=10,columnas=10;
+
+        // Instancias mis matrices
+        //int[] arreglo = new int[10] esto era el unidimensional
+        int[,] matrizA = new int[filas,columnas];
+        int[,] matrizB = new int[filas,columnas];
+
+        // Desplegar el menu
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("OPERACIONES DE MATRICES");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("1. RELLENAR MATRICES ALEATORIAMENTE");
+            Console.WriteLine("2. SUMAR MATRICES");
+            Console.WriteLine("3. NUMERO MAYOR DE AMBAS MATRICES");
+            Console.WriteLine("4. IMPRESION DE MATRICES");
+            Console.WriteLine("5. LA DIAGONAL PRINCIPAL DE AMBAS MATRICES");
+            Console.WriteLine("6. SALIR");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("INGRESE OPCION: ");
+            opc= Convert.ToInt32(Console.ReadLine());
+
+            //materializar el menu por medio de la estructura de control switch case
+            switch (opc)
+            {
+                case 1:
+                    FillMatrix(matrizA, matrizB);
+                    break;
+                case 2:
+                    SumMatrix(matrizA, matrizB);
+                    break;
+                case 3:
+                    Console.WriteLine("El mayor es: " + GetMaxNumber(matrizA, matrizB));  
+                    break;
+                case 4:
+                    PrintMatrix(matrizA, matrizB);
+                    break;
+                case 5:
+                    DiagonalMatrix(matrizA, matrizB);
+                    break;
+                case 6:
+                    Console.WriteLine("Adios, gracias por usar el programa");
+                    break;
+                default:
+                    Console.WriteLine("Opps algo salio mal, ingrese una opcion de nuevo");
+                    break;
+            }
+            Console.ReadKey();
+        } while (opc!=6);
+    }
+    //method
+    public static void FillMatrix(int[,] matrizA, int[,] matrizB){
+        int filas = matrizA.GetLength(0); 
+        int columnas = matrizA.GetLength(1); 
+        
+        // llenar aleatoreamente la matriz
+        Random rnd = new Random();
+        for (int i = 0; i < filas; i++) //filas
+        {
+            for (int j = 0; j < columnas; j++) //columnas
+            {
+                matrizA[i,j] = rnd.Next(0,100);
+                matrizB[i,j] = rnd.Next(0,100);
+            }
+        }
+        Console.WriteLine("MATRICES GENERADAS CON EXITO");
+    } 
+    //method
+    public static void SumMatrix(int[,] matrizA, int[,] matrizB){
+        // MATRIZ A
+        // 2    3   4
+        // 1    4   2
+
+        // MATRIZ B
+        //  3   1   4
+        //  4   1   2
+
+        // A + B
+        //  5   4   8
+        //  5   5   4
+
+        int sumaCeldasEquivalentes = 0;
+        int filas = matrizA.GetLength(0); 
+        int columnas = matrizA.GetLength(1);
+        Console.WriteLine("LA SUMA DE LAS MATRICES ES: ");
+        for (int i = 0; i < filas; i++) //filas
+        {
+            for (int j = 0; j < columnas; j++) //columnas
+            {
+                sumaCeldasEquivalentes = matrizA[i,j] + matrizB[i,j];
+                Console.Write(sumaCeldasEquivalentes + "\t"); //valor + TAB 
+            }
+            Console.WriteLine(""); //enter
+        }
+
+    }
+    //funcion
+    public static int GetMaxNumber(int[,] matrizA, int[,] matrizB){
+        int filas = matrizA.GetLength(0); 
+        int columnas = matrizA.GetLength(1);
+        int buffer = 0;
+
+        for (int i = 0; i < filas; i++) //filas
+        {
+            for (int j = 0; j < columnas; j++) //columnas
+            {
+                // DETERMINANDO EL VALOR MAYOR
+                if(buffer<matrizA[i,j])
+                    buffer =  matrizA[i,j];
+
+                if(buffer<matrizB[i,j])
+                    buffer =  matrizB[i,j];
+            }
+        }
+        return buffer;
+    }
+    //method
+    public static void PrintMatrix(int[,] matrizA, int[,] matrizB){
+        int filas = matrizA.GetLength(0); 
+        int columnas = matrizA.GetLength(1);
+
+        //imprimiendo matrix a
+        Console.WriteLine("");
+        Console.WriteLine("--------MATRIX A---------------");
+        Console.WriteLine("");
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                Console.Write(matrizA[i,j] + "\t");
+            }
+            Console.WriteLine("");
+        }
+        
+        Console.WriteLine("");
+        Console.WriteLine("--------MATRIX B---------------");
+        Console.WriteLine("");
+        //imprimiendo matrix b
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                Console.Write(matrizB[i,j] + "\t");
+            }
+            Console.WriteLine("");
+        }
+
+    }
+    public static void DiagonalMatrix(int[,] matrizA, int[,] matrizB){
+        //  0,0     0,1     0,2     0,3
+        //  1,0     1,1     1,2     1,3
+        //  2,0     2,1     2,2     2,3
+        //  3,0     3,1     3,2     3,3
+        int filas = matrizA.GetLength(0); 
+        int columnas = matrizA.GetLength(1);
+
+
+        //imprimiendo matrix a
+        Console.WriteLine("");
+        Console.WriteLine("--------MATRIX A---------------");
+        Console.WriteLine("");
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                if(i==j)
+                    Console.Write(matrizA[i,j] + "\t");
+                else
+                    Console.Write(0 + "\t");
+            }
+            Console.WriteLine("");
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("--------MATRIX B---------------");
+        Console.WriteLine("");
+        //imprimiendo matrix b
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                if(i==j)
+                    Console.Write(matrizB[i,j] + "\t");
+                else
+                    Console.Write(0 + "\t");
+            }
+            Console.WriteLine("");
+        }
+
+
+    }
+}
---
+++ semana13/arreglos_bidimensionales_secB1/Program.cs
@@ -0,0 +1,191 @@
+﻿using System; 
+public class Program
+{
+    public static void Main(){
+        // opc es el selector de nuestro menu
+        int opc = 0;
+
+        // Variables encargadas de la dimension de nuestra matriz n*m filas*columnas
+        int filas=10; // filas 
+        int columnas=10; // columnas
+        
+        // Instanciar matrices de 10*10 vacia
+        int[,] MatrizA = new int[filas,columnas];
+        int[,] MatrizB = new int[filas,columnas];
+        
+        // Desplegar el menu principal
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("OPERACIONES DE MATRICES");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("1. Rellenar Matrices Aleatoriamente");
+            Console.WriteLine("2. Suma de Matrices");
+            Console.WriteLine("3. Número Mayor de ambas matrices");
+            Console.WriteLine("4. Imprimir matrices");
+            Console.WriteLine("5. Imprimir la diagonal principal de ambas matrices");
+            Console.WriteLine("6. Imprimir la diagonal principal de ambas matrices");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Ingresar opción: ");
+            opc = Convert.ToInt32(Console.ReadLine());
+
+            switch (opc)
+            {
+                case 1:
+                    FillMatrix(MatrizA, MatrizB);
+                    break;
+                case 2:
+                    SumMatrix(MatrizA, MatrizB);
+                    break;
+                case 3:
+                    //llamado de funcion
+                    Console.WriteLine("El mayor es: " + GetMaxNumber(MatrizA, MatrizB));
+                    break;
+                case 4:
+                    PrintMatrix(MatrizA, MatrizB);    
+                    break;
+                case 5:
+                    DiagonalMatrix(MatrizA, MatrizB);    
+                    break;
+                case 6:
+                    Console.WriteLine("Adios, gracias por usar el programa");
+                    break;
+                default:
+                    Console.WriteLine("Opps algo salio mal, ingrese una opcion de nuevo");
+                    break;
+            }
+            Console.ReadKey();
+        } while (opc!=6);
+    }
+    public static void FillMatrix(int[,] MatrizA, int[,] MatrizB)
+    {
+        int filas = MatrizA.GetLength(0);
+        int columnas = MatrizA.GetLength(0);
+
+        Random rnd = new Random();
+        // forma # 1 , tengo variables donde almacené cuantas filas y cuantas columnas quiero
+        for (int i = 0; i < filas; i++) //filas
+        {
+            for (int j = 0; j < columnas; j++) // columnas
+            {
+                MatrizA[i,j] = rnd.Next(0,100);
+                MatrizB[i,j] = rnd.Next(0,100);
+            }
+        } 
+        Console.WriteLine("Matrices rellenadas correctamente");
+        /*
+        // forma # 2, le pregunto a la matriz cual es su tamaño
+        for (int i = 0; i < MatrizA.GetLength(0); i++) //filas
+        {
+            for (int j = 0; j < MatrizA.GetLength(1); j++) //columnas
+            {
+                MatrizA[i,j] = 0;
+                MatrizB[i,j] = 0;
+            }
+        }
+        */
+    }
+    //metodo
+    public static void SumMatrix(int[,] MatrizA, int[,] MatrizB)
+    {   
+        int filas = MatrizA.GetLength(0);
+        int columnas = MatrizA.GetLength(0);
+        int sumaCeldas = 0;
+
+        Console.WriteLine("RESULTADO DE SUMA DE MATRICES");
+        for (int i = 0; i < filas; i++) //filas
+        {
+            for (int j = 0; j < columnas; j++) // columnas
+            {
+                sumaCeldas = MatrizA[i,j] + MatrizB[i,j];
+                Console.Write(sumaCeldas + "\t");
+            }
+            Console.WriteLine("");
+        } 
+    }
+    //funcion
+    public static int GetMaxNumber(int[,] MatrizA, int[,] MatrizB)
+    {
+         int buffer = 0;
+         int filas = MatrizA.GetLength(0);
+         int columnas = MatrizA.GetLength(0);
+
+        //generando nueva matriz
+        for (int i = 0; i < MatrizA.GetLength(0); i++)
+        {
+            for (int j = 0; j < MatrizA.GetLength(1); j++)
+            {
+                if(buffer<MatrizA[i,j])
+                    buffer =  MatrizA[i,j];
+
+                if(buffer<MatrizB[i,j])
+                    buffer =  MatrizB[i,j];
+            }
+        }
+        return buffer;
+    }
+
+    //metodo
+    public static void PrintMatrix(int[,] MatrizA, int[,] MatrizB)
+    {
+        //imprimiendo matrix a
+        Console.WriteLine("");
+        Console.WriteLine("--------MATRIX A---------------");
+        Console.WriteLine("");
+        for (int i = 0; i < MatrizA.GetLength(0); i++) // filas 
+        {
+            for (int j = 0; j < MatrizA.GetLength(1); j++) // columnas
+            {
+                Console.Write(MatrizA[i,j] + "\t");
+            }
+            Console.WriteLine("");
+        }
+        Console.WriteLine("");
+        Console.WriteLine("--------MATRIX B---------------");
+        Console.WriteLine("");
+        //imprimiendo matrix b
+        for (int i = 0; i < MatrizB.GetLength(0); i++)
+        {
+            for (int j = 0; j < MatrizB.GetLength(1); j++)
+            {
+                Console.Write(MatrizB[i,j] + "\t");
+            }
+            Console.WriteLine("");
+        }
+    }
+    //metodo
+    public static void DiagonalMatrix(int[,] MatrizA, int[,] MatrizB)
+    {
+        //imprimiendo matrix a
+        Console.WriteLine("");
+        Console.WriteLine("--------MATRIX A---------------");
+        Console.WriteLine("");
+        for (int i = 0; i < MatrizA.GetLength(0); i++)
+        {
+            for (int j = 0; j < MatrizA.GetLength(1); j++)
+            {
+                if(i==j)
+                    Console.Write(MatrizA[i,j] + "\t");
+                else
+                    Console.Write(0 + "\t");
+            }
+            Console.WriteLine("");
+        }
+        Console.WriteLine("");
+        Console.WriteLine("--------MATRIX B---------------");
+        Console.WriteLine("");
+        //imprimiendo matrix b
+        for (int i = 0; i < MatrizB.GetLength(0); i++)
+        {
+            for (int j = 0; j < MatrizB.GetLength(1); j++)
+            {
+                if(i==j)
+                    Console.Write(MatrizB[i,j] + "\t");
+                else
+                    Console.Write(0 + "\t");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
+

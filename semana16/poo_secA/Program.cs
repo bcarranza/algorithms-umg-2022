efb762ac5818223fdf2c7642fcfffc973fb58860
bcarranza <bayron.carranza@gmail.com>
2022-10-21 15:08:23 -0600

    poo

---
+++ semana16/poo_secA/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+public class Animal
+{
+    public void Sleep()
+    {
+        Console.WriteLine("Zzzzzz");
+    }
+    public virtual void MakeNoise()
+    {
+        Console.WriteLine("Sonido Generico");
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeNoise()
+    {
+        Console.WriteLine("WoofWoof");
+    }
+}
+class Cat : Animal
+{
+    public override void MakeNoise()
+    {
+        Console.WriteLine("Meaaooooow");
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Instanciemos un nuevo perro");
+        Dog dog1 = new Dog();
+        Console.WriteLine("Ladrido del perro");
+        Console.WriteLine(dog1.MakeNoise);
+        Console.WriteLine("Perro anda dormi");
+        Console.WriteLine(dog1.Sleep);
+
+        Cat cat1 = new Cat();
+        Console.WriteLine("Maullido del perro");
+        Console.WriteLine(cat1.MakeNoise);
+        Console.WriteLine("Gato anda dormi");
+        Console.WriteLine(cat1.Sleep);
+
+        Console.ReadKey();
+
+        
+    }
+}
